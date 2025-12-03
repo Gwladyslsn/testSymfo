67083de0635385927efce880ed2aa9691f75d6a7
Gwladyslsn <gwladyslaisne@outlook.fr>
2025-12-03 16:38:36 +0100

    maj

--- testSymfony/src/Controller/RecipeController.php
+++ testSymfony/src/Controller/RecipeController.php
@@ -7,10 +7,12 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+
+class RecipeController extends AbstractController
 {
-    #[Route("/", name:"home")]
-    function index(Request $request): Response {
-        return new Response('Bonjour ' . $request->query->get('name', 'inconnu'));
+    #[Route("/recette/{slug}-{id}", name:"recipe.show")]
+    public function index(Request $request): Response
+    {
+        dd($request->attributes->get('slug'), $request->attributes->get('id'));
     }
 }
